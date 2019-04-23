dc5bd42e6c06cddb909725135f0c3053a536fadf
syn <vicdemilly@gmail.com>
2019-04-23 15:32:13 +0200

    Simplification du code et ajout du ready

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
-$('#lastName').on('focus', function(){
-  $(this).css('border', 'solid green 1px');
-})
-$('#firstName').on('focus', function(){
-  $(this).css('border', 'solid green 1px');
-})
-$('#lastName').on('blur', function(){
-  $(this).css('border', 'solid red 1px');
-})
-$('#firstName').on('blur', function(){
-  $(this).css('border', 'solid red 1px');
-})
+$(document).ready(function(){
+  $('input').on('focus', function(){
+    $(this).css('border', 'solid green 1px');
+  })
+  $('input').on('blur', function(){
+    $(this).css('border', 'solid red 1px');
+  })
+});
